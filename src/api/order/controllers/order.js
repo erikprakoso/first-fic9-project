39d46360837a9df3f636779b2b45d2d107b59cd5
Erik Prakoso <erikprakoso1993@gmail.com>
2023-10-26 19:41:01 +0700

    update order and payment callback

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,9 +1,41 @@
 'use strict';
 
+const { default: axios } = require('axios');
+
 /**
  * order controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController('api::order.order', ({ strapi }) => ({
+    async create(ctx) {
+        const result = await super.create(ctx);
+        console.log(result);
+
+        const { default: axios } = require('axios');
+        const { xenditHeaders } = require('../helpers/header.js');
+
+        console.log(xenditHeaders);
+
+        const payload = {
+            external_id: result.data.id.toString(),
+            payer_email: 'erikprakoso1993@gmail.com',
+            description: 'Payment for product',
+            amount: result.data.attributes.totalPrice
+        }
+
+        console.log(payload);
+
+        const response = await axios({
+            method: 'POST',
+            url: 'https://api.xendit.co/v2/invoices',
+            headers: xenditHeaders,
+            data: JSON.stringify(payload)
+        });
+
+        console.log(response);
+
+        return JSON.stringify(response.data);
+    }
+}));
